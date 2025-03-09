aaab780de4f460a66ffdb74c951f2bc298f1b8dc
yuting36 <yuting2014lim08@gmail.com>
2025-03-09 03:36:38 +0000

    Mar 9, 2025, 3:36 AM

--- CT06_07/test.py
+++ CT06_07/test.py
@@ -5,6 +5,6 @@ name = input("what is your name? ") # ask user for name
 print("Nice to meet you, " + name + "!") # greet the user
 
 # question 2
-# ask the user for satr
+# ask the user for start,
 start = input("give me a start number ")
 end = input("give me a end number ")
