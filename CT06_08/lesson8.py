5b04c426d801cc728eb5eb991bac5c7b12ee5489
yuting36 <yuting2014lim08@gmail.com>
2025-03-21 09:43:13 +0000

    Mar 21, 2025, 9:43 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -3,4 +3,4 @@ print("Hello from lesson 8")
 import random
 num1 = random.randint(1,50)
 num2 = random.randint(1,50)
-question = "what is " + str(num1) + "+ " + str(num)
+question = "what is " + str(num1) + "+ " + str(num2) +"?  "
