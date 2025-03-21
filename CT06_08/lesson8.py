620affde8db42e37f521e0dc814ad8d8b565d87e
yuting36 <yuting2014lim08@gmail.com>
2025-03-21 09:43:54 +0000

    Mar 21, 2025, 9:43 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -4,4 +4,5 @@ import random
 num1 = random.randint(1,50)
 num2 = random.randint(1,50)
 question = "what is " + str(num1) + "+ " + str(num2) +"?  "
-ans = int(input(quesstion))
+ans = int(input(question))
+
