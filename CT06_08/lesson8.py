e736fead8d07087c2c00ffdfe9e9c9b4d1ca02e5
yuting36 <yuting2014lim08@gmail.com>
2025-03-21 09:59:38 +0000

    Mar 21, 2025, 9:59 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -1,12 +1,12 @@
 print("Hello from lesson 8")
 
-import random
-num1 = random.randint(1,50)
-num2 = random.randint(1,50)
-question = "what is " + str(num1) + " + " + str(num2) +"?  "
-ans = int(input(question))
-hidden_ans = num1 + num2
-print( ans == hidden_ans )
+# import random
+# num1 = random.randint(1,50)
+# num2 = random.randint(1,50)
+# question = "what is " + str(num1) + " + " + str(num2) +"?  "
+# ans = int(input(question))
+# hidden_ans = num1 + num2
+# print( ans == hidden_ans )
         # or 
 # if ans == hidden_ans :
 #     print ("true")
