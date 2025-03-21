d96152e8a1a2f8c070374d6156b8c5f81ffcad19
yuting36 <yuting2014lim08@gmail.com>
2025-03-21 09:56:53 +0000

    Mar 21, 2025, 9:56 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -13,3 +13,11 @@ print( ans == hidden_ans )
 # else
 #     print ("false")
 
+
+import random
+num1 = random.randint(1,50)
+
+question = "what is " + str(num1) + " + " + str(num2) +"?  "
+ans = int(input(question))
+hidden_ans = num1 + num2
+print( ans == hidden_ans )
