6b7d082d969f7cc5ba1a229e67c4752077bb4a72
yuting36 <yuting2014lim08@gmail.com>
2025-03-23 03:29:10 +0000

    Mar 23, 2025, 3:29 AM

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -7,6 +7,6 @@ num2 = random.randint(1, 6)
 num3 = random.randint(1, 6)
 
 num1even = num1 % 2 == 0
-num1even = num1 % 2 == 0
-num2even = num1 % 2 == 0
+num2even = num2 % 2 == 0
+num3even = num1 % 2 == 0
 
