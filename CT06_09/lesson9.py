26d96d1419e45860ae2d816af568e388d5b98823
yuting36 <yuting2014lim08@gmail.com>
2025-03-23 03:33:38 +0000

    Mar 23, 2025, 3:33 AM

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -6,9 +6,9 @@ num1 = random.randint(1, 6)
 num2 = random.randint(1, 6)
 num3 = random.randint(1, 6)
 
-print("number one is :" + str(num1 ))
-print("number two is :" + str(num2 ))
-print("number three is :" + str(num3))
+print("number one is : " + str(num1 ))
+print("number two is : " + str(num2 ))
+print("number three is : " + str(num3))
 
 Even_odd = num1 % 2 == num2 % 2 == num3 % 2 == 0
 print (Even_odd)
