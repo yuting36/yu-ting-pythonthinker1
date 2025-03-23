e7d5736a8a514d74aeaf8c2fbd03146531b77087
yuting36 <yuting2014lim08@gmail.com>
2025-03-23 03:30:29 +0000

    Mar 23, 2025, 3:30 AM

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -5,8 +5,7 @@ import random
 num1 = random.randint(1, 6)
 num2 = random.randint(1, 6)
 num3 = random.randint(1, 6)
-
-num1even = num1 % 2 == num2 % 2 == num3 % 2 == 0
+Even_ = num1 % 2 == num2 % 2 == num3 % 2 == 0
 
 
 
