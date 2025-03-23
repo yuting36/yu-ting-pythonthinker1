3644b6fb7c2630edb4319a7b9aa81e01051bf381
yuting36 <yuting2014lim08@gmail.com>
2025-03-23 03:30:39 +0000

    Mar 23, 2025, 3:30 AM

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -5,7 +5,8 @@ import random
 num1 = random.randint(1, 6)
 num2 = random.randint(1, 6)
 num3 = random.randint(1, 6)
-Even_ = num1 % 2 == num2 % 2 == num3 % 2 == 0
+Even_odd = num1 % 2 == num2 % 2 == num3 % 2 == 0
+
 
 
 
