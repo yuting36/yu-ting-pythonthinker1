b457f3333e50386db0e038f1267f2c5cd29fec32
yuting36 <yuting2014lim08@gmail.com>
2025-03-30 04:18:32 +0000

    Mar 30, 2025, 4:18 AM

--- CT06_10/lesson10.py
+++ CT06_10/lesson10.py
@@ -33,5 +33,6 @@ if score >= 90 :
     print ("your grade is A")
 elif score >= 80:
     print ("your grade is B")
-elif score >= 80:
-    print ("your grade is B")
+elif score >= 70:
+    print ("your grade is C")
+    print ("your grade is ")
