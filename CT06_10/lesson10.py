97125800990303ad09496b2951128c4816951eea
yuting36 <yuting2014lim08@gmail.com>
2025-03-30 04:00:12 +0000

    Mar 30, 2025, 4:00 AM

--- CT06_10/lesson10.py
+++ CT06_10/lesson10.py
@@ -1,8 +1,9 @@
 # print("Hello from lesson 10")
 
-num = int(input("give me number now : "))
+# num = int(input("give me number now : "))
 
-if num > 0 :
-    print (str(num) + " is positive")
-else:
-    print (str(num) + " is negitive")
+# if num > 0 :
+#     print (str(num) + " is positive")
+# else:
+#     print (str(num) + " is negitive")
+
