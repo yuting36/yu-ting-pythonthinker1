25d6581a310670f8b123b6b7051722bf2594e5d6
yuting36 <yuting2014lim08@gmail.com>
2025-03-30 04:21:16 +0000

    Mar 30, 2025, 4:21 AM

--- CT06_10/lesson10.py
+++ CT06_10/lesson10.py
@@ -29,7 +29,9 @@ age = int(input("what is your age :"))
 
 score = int(input("what iz your score? :"))
 
-if score >= 90 :
+if score > 100:
+    print ("your grade is A+")
+elif score >= 90:
     print ("your grade is A")
 elif score >= 80:
     print ("your grade is B")
@@ -37,6 +39,6 @@ elif score >= 70:
     print ("your grade is C")
 elif score >= 60:
     print ("your grade is D")
-elif score < 60:
+else:
     print ("your grade is F")
  
