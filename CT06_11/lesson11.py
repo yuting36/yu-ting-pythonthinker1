dcd44cb340d4ecddf9e823c8aae0b9a1899f85b9
yuting36 <yuting2014lim08@gmail.com>
2025-04-06 03:12:20 +0000

    Apr 6, 2025, 3:12 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -5,4 +5,6 @@ px = int(input("how much $ iz the item? "))
 if px <= 5:
     print("might be bad quality but fine!")
 elif px >= 50:
-    print("good 2 buy:D")
+    print("good 2 buy :D")
+elif px >= 50:
+    print("good 2 buy :D")
