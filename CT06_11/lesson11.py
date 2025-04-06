ae3afdb820e05060e44ae218adba1c834d8fc0bb
yuting36 <yuting2014lim08@gmail.com>
2025-04-06 03:15:01 +0000

    Apr 6, 2025, 3:15 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -4,9 +4,9 @@ px = int(input("how much $ iz the item? "))
 
 if px <= 5:
     print("might be bad quality but fine!")
-elif px >= 50: # <=
+elif px <= 50: # <=
     print("good 2 buy :D")
-elif px >= 500: # <=
+elif px <= 500: # <=
     print("WHERE R U GETTING THIZ MUCH $ !!!????")
 else:
     print("R.I.P bank account :D")
