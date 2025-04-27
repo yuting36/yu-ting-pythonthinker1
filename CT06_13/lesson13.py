df924bedf7c3fda2e72715b74a0f7848aaf69dbe
yuting36 <yuting2014lim08@gmail.com>
2025-04-27 03:29:16 +0000

    Apr 27, 2025, 3:29 AM

--- CT06_13/lesson13.py
+++ CT06_13/lesson13.py
@@ -73,5 +73,4 @@ while True:
     if user_top == "end":
         break
     else:
-        user_top.append(pizza_top[int(choice) - 1
-                                  ])
+        user_top.append(pizza_top[int(choice) - 1])
