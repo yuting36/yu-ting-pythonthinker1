7660e0afaa6ca9f86ae1f649452b4970d6dd0308
yuting36 <yuting2014lim08@gmail.com>
2025-05-04 03:31:03 +0000

    May 4, 2025, 3:31 AM

---
+++ CT06_13/test.py
@@ -0,0 +1,24 @@
+#Task 1
+# Print numbers from 10 to 200 using the while loop
+# Your numbers must be in multiples of 10.
+# 10 must be first number printed, and 200 the last number.
+
+# Example: 10, 20, 30 ..... 180, 190, 200.
+# Note that the numbers do not need to be printed in one line.
+# Write your code here
+
+
+
+
+
+###################################################
+
+# Task 2
+# Code a password checker to protect your code!
+
+# Assign a password "superpass123" to a variable
+# Ask the user to enter a password
+# If the password matches, print “Access Granted”
+# If the password does not match, print “Access Denied”
+
+# Write your code here
