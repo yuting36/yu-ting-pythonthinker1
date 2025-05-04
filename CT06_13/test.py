10f45eb2831b78950879cc3231810531fc28ddd4
yuting36 <yuting2014lim08@gmail.com>
2025-05-04 03:50:28 +0000

    May 4, 2025, 3:50 AM

--- CT06_13/test.py
+++ CT06_13/test.py
@@ -56,7 +56,7 @@ planets.append("neptune")
 # 3. Elon Musk has conquered Mars. 
 #    Rename Mars in the list to be "muskworld"
 
-planets.pop([3] , "muskworld")
+planets[3]
 
 # 4. Remove uranus from this list.
 
