7c3ad0480ca9a5f0a1f2fde23ca7be1680b74cf5
yuting36 <yuting2014lim08@gmail.com>
2025-05-04 03:43:36 +0000

    May 4, 2025, 3:43 AM

--- CT06_13/test.py
+++ CT06_13/test.py
@@ -46,7 +46,7 @@ planets = ["mercury","venus", "earth", "mars", "jupiter", "saturn", "uranus" ]
 # 1. Write code below to print the 3rd item 
 #    in this list using index e.g. earth
 
-
+print()
 
 
 # 2. Write code to append neptune to this list.
