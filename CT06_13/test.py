38e99975a1f687a8c46615633f7e1ebd1e2913f4
yuting36 <yuting2014lim08@gmail.com>
2025-05-04 03:42:40 +0000

    May 4, 2025, 3:42 AM

--- CT06_13/test.py
+++ CT06_13/test.py
@@ -1,36 +1,61 @@
-#Task 1
-# Print numbers from 10 to 200 using the while loop
-# Your numbers must be in multiples of 10.
-# 10 must be first number printed, and 200 the last number.
+# #Task 1
+# # Print numbers from 10 to 200 using the while loop
+# # Your numbers must be in multiples of 10.
+# # 10 must be first number printed, and 200 the last number.
 
-# Example: 10, 20, 30 ..... 180, 190, 200.
-# Note that the numbers do not need to be printed in one line.
-# Write your code here
+# # Example: 10, 20, 30 ..... 180, 190, 200.
+# # Note that the numbers do not need to be printed in one line.
+# # Write your code here
 
-num = 10          
-while num <= 200: #loop it :D
-    print(num)    # print the number
-    num += 10     # make it the multiples of 10
-
-
+# num = 10          
+# while num <= 200: #loop it :D
+#     print(num)    # print the number
+#     num += 10     # make it the multiples of 10
 
 
 
-###################################################
 
-# Task 2
-# Code a password checker to protect your code!
 
-# Assign a password "superpass123" to a variable
-# Ask the user to enter a password
-# If the password matches, print “Access Granted”
-# If the password does not match, print “Access Denied”
+# ###################################################
 
-# Write your code here
+# # Task 2
+# # Code a password checker to protect your code!
 
-password = "superpass123" # the password
-user_pass = str(input("What is the password?  ")) # ask user to input password
+# # Assign a password "superpass123" to a variable
+# # Ask the user to enter a password
+# # If the password matches, print “Access Granted”
+# # If the password does not match, print “Access Denied”
 
-if user_pass == password:
-    print("Access Granted")
-else :print("Access Denied")
+# # Write your code here
+
+# password = "superpass123" # the password
+# user_pass = str(input("What is the password?  ")) # ask user to input password
+
+# if user_pass == password:
+#     print("Access Granted")
+# else :print("Access Denied")
+
+
+
+# Basic List Functions
+# Do the following operations to the list provided below
+# Write the code below each question.
+
+planets = ["mercury","venus", "earth", "mars", "jupiter", "saturn", "uranus" ]
+
+# 1. Write code below to print the 3rd item 
+#    in this list using index e.g. earth
+
+
+# 2. Write code to append neptune to this list.
+
+
+# 3. Elon Musk has conquered Mars. 
+#    Rename Mars in the list to be "muskworld"
+
+
+# 4. Remove uranus from this list.
+
+
+# 5. Using a for loop, print all the planets 
+#    from this list one by one.
