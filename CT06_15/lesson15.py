6ddad07eed51d44aab30b8e2b9c2b0e99573cdfc
yuting36 <yuting2014lim08@gmail.com>
2025-05-11 03:49:56 +0000

    May 11, 2025, 3:49 AM

--- CT06_15/lesson15.py
+++ CT06_15/lesson15.py
@@ -2,6 +2,10 @@ print("Hello from lesson 14")
 
 counter = 0
 
-increment_counter(counter =+ 1)
+def increment_counter():
+    global counter
+    counter += 1
 
-
+for i in range (3):
+    
+    increment
