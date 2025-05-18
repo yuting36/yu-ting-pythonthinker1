886ce5601521a59123542ecd79f836e099df024c
yuting36 <yuting2014lim08@gmail.com>
2025-05-18 03:35:39 +0000

    May 18, 2025, 3:35 AM

--- CT06_16/lesson16.py
+++ CT06_16/lesson16.py
@@ -3,21 +3,21 @@ print("Hello from lesson 16")
 import turtle
 t = turtle.Turtle()
 
-window = t.screen
-window.setup(width=600 , height=400)
+# window = t.screen
+# window.setup(width=600 , height=400)
 
-def shape(length,sides):
-    t.pendown
-    t.seth(90)
-    for i in range (sides):
-         t.forward(length)
-         t.left(360 / sides)
+# def shape(length,sides):
+#     t.pendown
+#     t.seth(90)
+#     for i in range (sides):
+#          t.forward(length)
+#          t.left(360 / sides)
 
-num = 0.1
-while num != 10:
-    shape(num,100)
-    num += 0.1
+# num = 0.1
+# while num != 10:
+#     shape(num,100)
+#     num += 0.1
 
-window.mainloop()
+# window.mainloop()
 
-print("sucess :D")
+# print("sucess :D")
