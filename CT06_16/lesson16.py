b63273ec2226dc3b6e1a9d59507ec16d3a2ebc72
yuting36 <yuting2014lim08@gmail.com>
2025-05-18 03:22:45 +0000

    May 18, 2025, 3:22 AM

--- CT06_16/lesson16.py
+++ CT06_16/lesson16.py
@@ -9,7 +9,7 @@ window.setup(width=600 , height=400)
 def shape(length,sides):
     t.pendown
     t.seth(90)
-    for i in range (4):
+    for i in range (sides):
          t.forward(length)
          t.left( 180 - 360 / sides)
 
